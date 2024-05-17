53e54e5e83cbfdbc2d63f66db28ef111ae78cc0e
Yocary_CD <90988723+yocary@users.noreply.github.com>
2024-05-17 12:51:17 -0600

    Se realizan modificaciones en el frontend
    Se realizan modificaciones en el frontend

--- Componentes/Frontend/karma.conf.js
+++ Componentes/Frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sdr'),
+      dir: require('path').join(__dirname, './coverage/sistema-control-turnos'),
       subdir: '.',
       reporters: [
         { type: 'html' },
